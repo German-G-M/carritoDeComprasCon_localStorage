1626f6874a4f4c6e04f8e65a2db09f6173bbe609
German-G-M <germangutierrrez.ggm@gmail.com>
2022-07-25 21:13:37 -0400

    carrito de compras con localStorage  funcionando

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+//función autoejecutable
+(function prueba (e){
+    alert("hola carambola desde dentro de la funcio autoejecutable")
+})()
+*/
+
+//alert("hola")
+console.log(localStorage)
+//localStorage.setItem("producto","carne chuleta");
+//localStorage.setItem("producto2","molida corriente");
+//localStorage.setItem("producto3","pulpa");
+//localStorage.setItem("producto4","costilla");
+
+/*
+//para listar todos los datos del local storage
+for (let index = 0; index < localStorage.length; index++) {
+    llave = localStorage.key(index)//obtiene el nombre de la llave
+    console.log(`[${index+1}]--> ${llave} : ${localStorage.getItem(llave)}`)//muestro el nombre de la llave y sus datos
+    
+}
+*/
